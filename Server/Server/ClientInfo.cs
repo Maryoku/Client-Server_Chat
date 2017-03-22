0b3627061cc99ca74892539ffeaf37382d3008f5
Maryoku <maryoku.amaya@gmail.com>
2017-03-23 03:06:45 +0500

    add class for clients

---
+++ Server/Server/ClientInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class ClientInfo
+    {
+        public TcpClient Client;
+        public List<byte> buffer = new List<byte>();
+        public bool IsConnect;
+
+        public ClientInfo(TcpClient Client)
+        {
+            this.Client = Client;
+            IsConnect = true;
+        }
+    }
+}
